f30fab2cec7d0909e01d799260e5a5be07709781
ayourelwazani97 <ayoubwazane306@gmail.com>
2024-01-06 13:58:35 +0100

    ordering data by date

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php 
+//connect to database
+$connection = mysqli_connect("localhost" /*host*/,"ayoub"  /*username*/,"ayoub123" /*password*/,"ninjapizza" /*dbname*/);
+
+//check connection
+if(!$connection) {
+    //mysqli_connect_error() => to display error of database
+    echo "connection error : " . mysqli_connect_error();
+}
+
+//write query for all pizzas
+$sql = "SELECT title,id,ingredients FROM pizzas ORDER BY created_at"; // SELECT * used to select all from our table pizzas
+//OREDER BY its for ordering data 
+//make query and get results
+
+$res = mysqli_query($connection, $sql);
+
+//fetch results as an array
+
+$pizzas = mysqli_fetch_all($res ,MYSQLI_ASSOC);
+
+mysqli_free_result($res);
+
+//close connection
+mysqli_close($connection);
+
+print_r($pizzas);
+
 
 ?>
 

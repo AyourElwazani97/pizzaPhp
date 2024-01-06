6fb62ba5dee6d21a6e835399df6cdf859280c69a
ayourelwazani97 <ayoubwazane306@gmail.com>
2024-01-06 16:32:22 +0100

    done with form now

--- templates/form.php
+++ templates/form.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("config/db_connection.php");
 /*
 $errors = array("email"=>"","title"=>"","ingredients"=>"");
@@ -8,31 +8,45 @@ $errors = array("email"=>"","title"=>"","ingredients"=>"");
         echo $_GET["ingredients"];
     }
     */
-    //using POST method
-    $errors = array("email"=>"","title"=>"","ingredients"=>"");
-    if(isset($_POST["submit"])) {
-        echo $_POST["email"];
-        echo $_POST["title"];
-        //echo htmlspecialchars($_POST["ingredients"]); //htmlspec... is used to prevent XSS attacks
-        //form validation
-        if(empty($_POST["email"]) || empty($_POST["title"]) ) {
-            echo "please fill all the fields";
-        }else {
-            if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-                //echo "invalid mail";
-                 $errors["email"] = "ooops something went wrong" . "<br/>";
-                //FILTER_VALIDATE_EMAIL its a filter for email
+//using POST method
+$errors = array("email" => "", "title" => "", "ingredients" => "");
+if (isset($_POST["submit"])) {
+    echo $_POST["email"];
+    echo $_POST["title"];
+    //echo htmlspecialchars($_POST["ingredients"]); //htmlspec... is used to prevent XSS attacks
+    //form validation
+    if (empty($_POST["email"]) || empty($_POST["title"])) {
+        echo "please fill all the fields";
+    } else {
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            //echo "invalid mail";
+            $errors["email"] = "ooops something went wrong" . "<br/>";
+            //FILTER_VALIDATE_EMAIL its a filter for email
+        }
+        if (array_filter($errors)) {
+            echo "there is errors";
+        } else {
+            //redirect user => header('location:index.php');
+            //submitting data
+            $email = mysqli_escape_string($connection, $_POST["email"]);
+            $title = mysqli_escape_string($connection, $_POST["title"]);
+            $ingredients = mysqli_escape_string($connection, $_POST["ingredients"]);
+        
+            //create sql
+            $sql = "INSERT INTO pizzas(title,email,ingredients) VALUES('$title','$email','$ingredients')";
+        
+            //save todb and check
+            if (mysqli_query($connection, $sql)) {
+            header('location:index.php');
+            } else {
+                echo "connection error : " . mysqli_error($connection);
             }
         }
     }
-    //if success and no error let's redirect the user
-    //array_filter($errors) this method is for checking if there is no erros in our array since array if empty is good
-    if(array_filter($errors)) {
-        echo "there is errors";
-    }else {
-        //redirect user => header('location:index.php');
-        //submitting data
-    }
+}
+//if success and no error let's redirect the user
+//array_filter($errors) this method is for checking if there is no erros in our array since array if empty is good
+
 
 ?>
 <section class="container grey-text">
@@ -41,15 +55,15 @@ $errors = array("email"=>"","title"=>"","ingredients"=>"");
     </h4>
     <form action="add.php" class="white" method="POST">
         <div class="red-text">
-        <?php 
+            <?php
             echo $errors["email"]
-        ?>
+            ?>
         </div>
-        <label >Email : </label>
+        <label>Email : </label>
         <input type="text" name="email">
-        <label >Pizza Title : </label>
+        <label>Pizza Title : </label>
         <input type="text" name="title">
-        <label >Ingredients : </label>
+        <label>Ingredients : </label>
         <input type="text" name="ingredients">
         <div class="center">
             <input type="submit" value="submit" name="submit" class="btn brand z-depth-0">

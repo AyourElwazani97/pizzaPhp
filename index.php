b922a38e07cd7250bb32d8cea8ba0dfcaba07b11
ayourelwazani97 <ayoubwazane306@gmail.com>
2024-01-06 14:49:09 +0100

    updating way of using foreach"

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $exp = explode(",", $pizzas[0]["ingredients"]);
 <?php @include("./templates/header.php"); ?>
 <h4 class="center grey-text">Pizzas !</h4>
 <div class="container">
-    <?php foreach ($pizzas as $pizza) { ?>
+    <?php foreach ($pizzas as $pizza) : ?>
         <div class="col s2 md3">
             <div class="card z-depth-0">
                 <div class="card-content center">
@@ -45,11 +45,11 @@ $exp = explode(",", $pizzas[0]["ingredients"]);
                     </h6>
                     <div>
                         <ul>
-                            <?php  foreach (explode("," , $pizza["ingredients"]) as $ing); {?>
+                            <?php  foreach (explode("," , $pizza["ingredients"]) as $ing) :?>
                                 <li>
                                     <?php echo htmlspecialchars($ing); ?>
                                 </li>
-                            <?php }?>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                 </div>
@@ -58,7 +58,7 @@ $exp = explode(",", $pizzas[0]["ingredients"]);
                 </div>
             </div>
         </div>
-    <?php } ?>
+    <?php endforeach; ?>
 
 </div>
 <?php @include("./templates/footer.php"); ?>

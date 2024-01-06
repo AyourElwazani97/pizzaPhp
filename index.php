a80cdac1dd6dcc5d60524eb16dfa906044e37dd9
ayourelwazani97 <ayoubwazane306@gmail.com>
2024-01-06 14:08:43 +0100

    rendering content

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 //connect to database
-$connection = mysqli_connect("localhost" /*host*/,"ayoub"  /*username*/,"ayoub123" /*password*/,"ninjapizza" /*dbname*/);
+$connection = mysqli_connect("localhost" /*host*/, "ayoub"  /*username*/, "ayoub123" /*password*/, "ninjapizza" /*dbname*/);
 
 //check connection
-if(!$connection) {
+if (!$connection) {
     //mysqli_connect_error() => to display error of database
     echo "connection error : " . mysqli_connect_error();
 }
@@ -17,23 +17,40 @@ $res = mysqli_query($connection, $sql);
 
 //fetch results as an array
 
-$pizzas = mysqli_fetch_all($res ,MYSQLI_ASSOC);
+$pizzas = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
 mysqli_free_result($res);
 
 //close connection
 mysqli_close($connection);
 
-print_r($pizzas);
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<?php 
-    @include("./templates/header.php");
-    @include("./templates/footer.php");
-?>
+<?php @include("./templates/header.php"); ?>
+<h4 class="center grey-text">Pizzas !</h4>
+<div class="container">
+    <?php foreach ($pizzas as $pizza) { ?>
+        <div class="col s2 md3">
+            <div class="card z-depth-0">
+                <div class="card-content center">
+                    <h6>
+                        <?php echo htmlspecialchars($pizza["title"]); ?>
+                    </h6>
+                    <div>
+                        <?php echo htmlspecialchars($pizza["ingredients"]); ?>
+                    </div>
+                </div>
+                <div class="card-action right-align">
+                    <a href="#" class="brand-text">more info</a>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+
+</div>
+<?php @include("./templates/footer.php"); ?>
+
 </html>
